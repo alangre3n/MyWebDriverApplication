d64191ae52f90e9955b0ff7749aac7f11b9edffe
machettie <55490804+machettie@users.noreply.github.com>
2020-09-23 00:12:10 +0400

    update .gitignore

--- src/main/java/ru/dexsys/App.java
+++ src/main/java/ru/dexsys/App.java
@@ -3,17 +3,9 @@ package ru.dexsys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) {
-        //System.setProperty("webdriver.chrome.driver", "C:/Program Files/Web Driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         MainPage page = new MainPage(driver);
         page.clickToHomeTitle();
